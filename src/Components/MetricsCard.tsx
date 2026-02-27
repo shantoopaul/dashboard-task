481fb7cc46a37f7485e37786bb79105b7cc812d1
shantoopaul <shantoopaul@gmail.com>
2026-02-27 19:04:16 +0600

    add metrics cards

---
+++ src/Components/MetricsCard.tsx
@@ -0,0 +1,92 @@
+import { ArrowUpRight } from "lucide-react";
+
+interface MetricsCardProps {
+  title: string;
+  count: number;
+  change?: number;
+  statusText?: string;
+  variant?: "default" | "primary";
+}
+
+const MetricsCard = ({
+  title,
+  count,
+  change,
+  statusText,
+  variant = "default",
+}: MetricsCardProps) => {
+  const isPrimary = variant === "primary";
+  const isPositive = typeof change === "number" && change > 0;
+  const isNegative = typeof change === "number" && change < 0;
+
+  return (
+    <section
+      className={`
+        rounded-2xl p-6 transition-all duration-300
+        ${
+          isPrimary
+            ? "bg-linear-to-b from-green3 to-green2 text-white"
+            : "bg-white"
+        }
+      `}
+    >
+      <div className="flex items-start justify-between">
+        <span
+          className={`text-xl font-medium ${isPrimary ? "text-white/90" : "text-gray-800"}`}
+        >
+          {title}
+        </span>
+
+        <div
+          className={`
+            w-9 h-9 flex items-center justify-center rounded-full border
+            ${
+              isPrimary
+                ? "bg-white text-black border-white"
+                : "border-gray-400 text-gray-700"
+            }
+          `}
+        >
+          <ArrowUpRight size={20} />
+        </div>
+      </div>
+
+      <h3
+        className={`mt-2 text-6xl font-medium ${isPrimary ? "text-white" : "text-black"}`}
+      >
+        {count}
+      </h3>
+
+      <div className="mt-4 flex items-center gap-2">
+        {typeof change === "number" ? (
+          <>
+            <div
+              className={`
+                flex items-center gap-1 px-2 py-0.75 rounded-md text-xs font-semibold border
+                ${isPositive && "bg-green3/10 text-green1 border-green1"}
+                ${isNegative && "bg-red-100 text-red-400 border-red-400"}
+              `}
+            >
+              {Math.abs(change)}
+              {isPositive && (
+                <div className="w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-b-6 border-green1"></div>
+              )}
+              {isNegative && (
+                <div className="w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-t-6 border-red-400"></div>
+              )}
+            </div>
+
+            <span className="text-sm text-green1">
+              {isPositive && "Increased"}
+              {isNegative && "Decreased"} from last month
+            </span>
+          </>
+        ) : (
+          <span className="text-sm text-green1">{statusText}</span>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default MetricsCard;
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,9 +1,28 @@
 import DashTopStuff from "../Components/DashTopStuff";
+import MetricsCard from "../Components/MetricsCard";
 
 const Dashboard = () => {
   return (
     <>
       <DashTopStuff />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        <MetricsCard
+          title="Total Projects"
+          count={24}
+          change={5}
+          variant="primary"
+        />
+
+        <MetricsCard title="Ended Projects" count={10} change={6} />
+
+        <MetricsCard title="Running Projects" count={12} change={2} />
+
+        <MetricsCard
+          title="Pending Project"
+          count={2}
+          statusText="On Discuss"
+        />
+      </div>
     </>
   );
 };
